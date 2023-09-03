ff89ff44b611448fcdbef84549869bbf4d6a4d66
Vinayak <vinayakdeepu@gmail.com>
2023-09-03 22:58:07 +0530

    Use new lib, feature to update anon key

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import { SearchDialog } from "vin-search-test";
+import { useState } from "react";
+import { SearchDialog } from "vin-vite-lib";
 
 function App() {
+  const [anonKey, setAnonKey] = useState("");
+
   return (
     <div className="main">
       <p className="name">Queryflow Test App</p>
-      <SearchDialog anonKey="6d188a4dc616e6c8a380c49f98e5f295dcbc634ade61bd20044318e0b03ef515" />
+      <div>
+        <label>Anon Key: </label>
+        <input
+          className="anon-key-input"
+          type="text"
+          placeholder="anon key"
+          value={anonKey}
+          onChange={(e) => setAnonKey(e.target.value)}
+        />
+      </div>
+      <SearchDialog anonKey={anonKey} />
     </div>
   );
 }
